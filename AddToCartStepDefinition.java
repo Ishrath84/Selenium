4790333157c442bd73ed39ae27d06bfd9be9def2
Ishrath84 <167670066+Ishrath84@users.noreply.github.com>
2024-04-21 13:49:40 +0530

    Create AddToCartStepDefinition.java

---
+++ AddToCartStepDefinition.java
@@ -0,0 +1,42 @@
+package automationexercise.stepdefs;
+
+public class AddToCartStepDefinition extends RunnerTest{
+
+AddToCart addtoCartPage = new AddToCart(getDriver());
+
+//Launch the Application
+@Given("Launch Automation Exercise Application")
+public void launchapplication() throws Exception {
+addtoCartPage.launchApplication();
+}
+
+//Select Produt in PLP Page
+@When("Select desired product for Product Listing Page")
+public void selectProductinPLP() throws Exception {
+addtoCartPage.selectCategory();
+addtoCartPage.selectProductinPlPage();
+addtoCartPage.selectProductinPlPage();
+}
+
+//Verify Product is Added to Cart
+@Then("Verify Add to Cart Popup")
+public void AddtoCartPopup() throws Exception {
+addtoCartPage.verifyAddtoCartPopupDetails();
+}
+
+//Verify Product Details in Shopping Cart Page
+@Then("Verify Product details in Shopping Cart Page")
+public void ProductDetailsinShoppingCartPage() throws Exception {
+addtoCartPage.verifyProductDetailsinShoppingCartPage();
+}
+
+//Remove Product from cart
+@When("Remove Product in Shopping Cart Page")
+public void RemoveProductinShoppingCartPage() throws Exception {
+addtoCartPage.removeProductinShoppingCartPage();
+}
+//Verify Empty Cart 
+@Then("Verify Empty Cart Page Details")
+public void VerifyEmptyCart() throws Exception {
+addtoCartPage.verifyEmptyCartPage();
+}
